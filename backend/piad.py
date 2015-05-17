668a7675929cb997b450d97e04d8d085744b9d17
Adam MacLeod <adam@macleod.id.au>
2015-05-17 11:03:19 +1000

    Updated backend service

--- backend/piad.py
+++ backend/piad.py
@@ -3,6 +3,9 @@ from lxml import html
 import requests
 
 app = Flask(__name__)
+kubernetes_url = '128.199.235.232:8080'
+dockci_url = 'paas.dock.ci'
+
 
 @app.route("/api/")
 def index():
@@ -10,100 +13,122 @@ def index():
     return jsonify(**result)
 
 @app.route("/api/apps")
-def apps():
-    page = requests.get('http://paas.dock.ci/')
+def applications():
+    """
+    Lists all applications by scraping DockCI
+    :return:
+    """
+    page = requests.get('http://%s/' % dockci_url)
     tree = html.fromstring(page.text)
 
-    apps = [
+    applications = [
         {
             'id': a.attrib['href'][6:],
             'name': a.text
         } for a in tree.xpath('//table/tr/td/a')]
 
     result = {
-        'data': apps
+        'data': applications
     }
     return jsonify(**result)
 
 
-@app.route("/api/pipeline/deploys")
-def deploys():
+@app.route("/api/apps/<appid>")
+def application(appid):
+    """
+    Collectis information on the status of one application - including current deploy and a list of historical deploys.
+    :param appid:
+    :return:
+    """
+    page = requests.get('http://%s/jobs/%s' % (dockci_url, appid))
+    tree = html.fromstring(page.text)
+
+    deploys = []
+    for a in tree.xpath('//tr[@class="build-detail"]'):
+        deploy = {
+            'created': a[0][0].text,
+            'tag': a[1].text,
+            'commit': a[2].text,
+            'author': a[3][0][0].text,
+            'state': a[4][0].attrib['title']
+        }
+        deploys.append(deploy)
+
+
+    first_deploy_id = [a.attrib['href'] for a in tree.xpath('//tr[@class="build-detail"]/td/a')][0]
+    current_deploy = requests.get('http://%s%s.json' % (dockci_url, first_deploy_id)).json()
+
+    # Status Handling
+    # stages: Shows the current stage we are up to on the display, could be one of:
+    #         start, local, git, repo, dockci, registry, chronos, kubernetes, done
+    # status: Dictionary containing tick/cross for every stage, not having a key will not show anything. ie:
+    #         {'repo': 'tick', 'dockci':'tick', 'registry':'cross'}
+    stage = "repo"
+    status = {
+        'repo': 'tick',
+    }
+
+    # If you're doing docker_push then you've made it past DockCI
+    if 'docker_push' in current_deploy['build_stage_slugs']:
+        stage = "dockci"
+        status['dockci'] = 'tick'
+
+    # If you've made it to docker_fetch then you've done the docker_push, meaning it's in the registry!
+    if 'docker_fetch' in current_deploy['build_stage_slugs']:
+        stage = "registry"
+        status['registry'] = 'tick'
+
+    # TODO: This was some last minute hacky stuff - the logic works (I think) but is confusing.
+    # if the overall result is in the message then check for success/fail
+    if 'result' in current_deploy:
+        if current_deploy['result'] == "success":
+            status['dockci'] = 'tick'
+            status['registry'] = 'tick'
+        elif current_deploy['result'] == "error":
+            # This could actually fail at registry or dockci - needs better logic here to determine. Assume fail at reg.
+            stage = 'registry'
+            status['dockci'] = 'tick'
+            status['registry'] = 'cross'
+
+    # TODO: Code for replication controllers is not finished - need to choose correct controller somehow and then
+    #       determine the correct attribute to know whether the new services are up.
+    reps = requests.get('http://%s/api/v1beta3/replicationcontrollers' % kubernetes_url, verify=False)
+
+    rep = reps.json()['items'][0] # TODO: Choose the right rep controller
+
+    if rep['status']['replicas'] > 0: # TODO: Check if this is the right metric for determining if it's running!
+        pass
+        # status = 'done'
+
+    #Return Result
     result = {
-        'data': [
-            {
-                'commit': {
-                    'hash': '81dfb16b33ec2671af9abd6c2b290644a6b23e14',
-                    'author': 'adam@macleod.id.au'
-                },
-                'git': {
-                    'status': 'Complete',
-                    'detailed': 'Git More information'
-                },
-                'repo': {
-                    'status': 'Complete',
-                    'detailed': 'Repo More information'
-                },
-                'dockci': {
-                    'status': 'Complete',
-                    'detailed': 'DockCI More information'
-                },
-                'registry': {
-                    'status': 'Complete',
-                    'detailed': 'Registry More information'
-                },
-                'chronos': {
-                    'status': 'Complete',
-                    'detailed': 'Chronos More information'
-                },
-                'kubernetes': {
-                    'status': 'Pending',
-                    'detailed': 'Kubernetes More information'
-                }
-            },
-            {
-                'commit': {
-                    'hash': '9093d90f87dc33aa3609160872acccc36ce950a1',
-                    'author': 'ricky@thatpanda.com'
-                },
-                'git': {
-                    'status': 'Complete',
-                    'detailed': 'More information'
-                },
-                'repo': {
-                    'status': 'Complete',
-                    'detailed': 'More information'
-                },
-                'dockci': {
-                    'status': 'Complete',
-                    'detailed': 'More information'
-                },
-                'registry': {
-                    'status': 'Complete',
-                    'detailed': 'More information'
-                },
-                'chronos': {
-                    'status': 'Complete',
-                    'detailed': 'More information'
-                },
-                'kubernetes': {
-                    'status': 'Pending',
-                    'detailed': 'More information'
-                }
-            }
-        ]
+        'current_deploy': {
+            'stage': stage,
+            'status': status
+        },
+        'deploys': deploys
     }
     return jsonify(**result)
 
 @app.route("/api/kubernetes/nodes")
 def nodes():
+    """
+    Passes through the kubernetes nodes endpoint
+    :return:
+    """
     r = requests.get('http://localhost:8080/api/v1beta3/nodes', verify=False)
     return jsonify(**r.json())
 
 @app.route("/api/kubernetes/pods")
 def pods():
+    """
+    Passes through the kubernetes nodes endpoint
+    :return:
+    """
     r = requests.get('http://localhost:8080/api/v1beta3/pods', verify=False)
     return jsonify(**r.json())
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
